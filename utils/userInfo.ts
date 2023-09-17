0679efbbe305bdc98da68e460c9ca7db57203e32
Bryan Thomas <thomas.bryan.m@gmail.com>
2023-09-17 12:11:13 -0700

    update queries

--- utils/userInfo.ts
+++ utils/userInfo.ts
@@ -1,16 +1,46 @@
 import { getDatabaseContainer } from "./database";
 
-export type Subscription = ""| "STARTER" | "PRO" | "TEAM" | "ENTERPRISE";
+export type Subscription = "" | "STARTER" | "PRO" | "TEAM" | "ENTERPRISE";
 
-export const getSubscription = async (userId:string, orgId) => {
+export const getSubscription = async (userId: string, orgId) => {
     if (!orgId) { // If there is no orgId, then the user is not part of an organization
         const container = await getDatabaseContainer("Users");
-        const { resource: user } = await container.item(userId, undefined).read();
-        return user.subscription as Subscription;    
+        let querySpec = {
+            query: "SELECT * FROM c WHERE c.userId = @userId",
+            parameters: [
+                {
+                    name: "@userId",
+                    value: userId
+                }
+            ]
+        };
+        const { resources: users } = await container.items
+            .query(querySpec)
+            .fetchAll();
+        if (users.length === 0) {
+            throw new Error("User not found");
+        }
+        const user = users[0];
+        return user.subscription as Subscription;
     }
-    else{ // If there is an orgId, then the user is part of an organization get the subscription from the organization
+    else { // If there is an orgId, then the user is part of an organization get the subscription from the organization
         const container = await getDatabaseContainer("Organizations");
-        const { resource: org } = await container.item(orgId, undefined).read();
+        let querySpec = {
+            query: "SELECT * FROM c WHERE c.orgId = @orgId",
+            parameters: [
+                {
+                    name: "@orgId",
+                    value: orgId
+                }
+            ]
+        };
+        const { resources: orgs } = await container.items
+            .query(querySpec)
+            .fetchAll();
+        if (orgs.length === 0) {
+            throw new Error("Organization not found");
+        }
+        const org = orgs[0];
         return org.subscription as Subscription;
     }
 };
